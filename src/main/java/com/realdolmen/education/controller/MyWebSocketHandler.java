e9e58a011bea3a9c05c1a13af3729bb217f8a45b
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-09-28 16:44:05 +0200

    Store a list of WebSocketSession instances to emulate a chat app

--- src/main/java/com/realdolmen/education/controller/MyWebSocketHandler.java
+++ src/main/java/com/realdolmen/education/controller/MyWebSocketHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Simple SocketHandler which echo's every input back to the client.
@@ -16,24 +18,36 @@ import java.io.IOException;
  */
 public class MyWebSocketHandler extends TextWebSocketHandler {
 
+    private List<WebSocketSession> sessions = new ArrayList<>();
+
     private Logger logger = LoggerFactory.getLogger(MyWebSocketHandler.class);
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         super.afterConnectionEstablished(session);
         logger.info("Incoming connection from " + session.getRemoteAddress().toString());
+        sessions.add(session);
     }
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        // Servlet alike system: A single WebSocketHandler handles many requests.
+        logger.info(Integer.toString(this.hashCode()));
+
         logger.info(message.getPayload());
+
 
         if(message.getPayload().isEmpty()){
             session.sendMessage(new TextMessage("Closing Connection"));
             session.close();
         }
         else {
-            session.sendMessage(new TextMessage(message.getPayload()));
+            for(WebSocketSession aSession : sessions) {
+                if(aSession.isOpen()) { // Not a complete solution.
+                                        // Thread may be suspended after this call and then the session may be closed.
+                    aSession.sendMessage(new TextMessage(message.getPayload()));
+                }
+            }
         }
     }
 
@@ -41,5 +55,7 @@ public class MyWebSocketHandler extends TextWebSocketHandler {
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
         logger.info("Closing Connection!");
+        sessions.remove(session);
+        logger.info("Number of connections: " + sessions.size());
     }
 }
